d264c11a56298cfeef173b658fe875a83fb5e7bd
vishaljiandani <vishal.jiandani18@gmail.com>
2022-11-08 08:38:53 -0700

    initial commit

---
+++ query.sql
@@ -0,0 +1,17 @@
+/* What are the top 5 brands by receipts scanned for most recent month? */
+/* Assumption: Taking "most recent month" to mean the previous month*/
+
+with cte as (
+  select b.name
+        ,count(distinct r._id) as receipts_scanned
+        from brands b
+        join items i on i.barcode = b.barcode
+        join receipts r on r._id = i.receipt_id
+        where datepart(m, dateScanned) = datepart(m, dateadd(m, -1, getdate()))
+        and datepart(yyyy, dateScanned) = datepart(yyyy, dateadd(m, -1, getdate()))
+        group by b.name
+)
+
+select top 5 name as top_5_brands
+from cte
+order by receipts_scanned desc
